e8cbe6900e4197b8f549fc1f65ede571c23505fa
RathomBoii <jessada.srmmn@gmail.com>
2025-05-05 16:55:57 +0700

    fix invoice and delivery note ref on backlog

--- odoo17-custom-addons/process_tracker/models/backlog.py
+++ odoo17-custom-addons/process_tracker/models/backlog.py
@@ -1,7 +1,5 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
-import pytz
-from datetime import datetime, time
 
 class ProcessBacklog(models.Model):
     _name = 'process.backlog'
@@ -14,17 +12,18 @@ class ProcessBacklog(models.Model):
     status = fields.Selection([
         ('order_received', 'Order Received'), ('transitioned', 'Transitioned')
     ], default='pending')
-    invoice_id = fields.Many2one('account.move', domain=[('move_type', '=', 'out_invoice')], readonly=True)
-    delivery_id = fields.Many2one('stock.picking', domain=[('picking_type_code', '=', 'outgoing')], 
-                            readonly=True, string="Delivery Note")
+    invoice_id = fields.Many2one('account.move',  string="Invoice ID", compute="_compute_invoice_delivery")
+    delivery_id = fields.Many2one('stock.picking',  string="Delivery Note", compute="_compute_invoice_delivery")
 
+    @api.depends('sale_order_id.invoice_ids', 'sale_order_id.picking_ids')
+    def _compute_invoice_delivery(self):
+        for record in self:
+            invoice = record.sale_order_id.invoice_ids.filtered(lambda x: x.move_type == 'out_invoice')[:1]
+            delivery = record.sale_order_id.picking_ids.filtered(lambda x: x.picking_type_code == 'outgoing')[:1]
+            record.invoice_id = invoice.id if invoice else False
+            record.delivery_id = delivery.id if delivery else False
 
     def write(self, vals):
-
-        # get current status
-        # get new status (val)
-        # conditioning process
-
 
         result = super().write(vals)  # Update status first
         if vals.get('status') == 'transitioned':
@@ -57,22 +56,6 @@ class ProcessBacklog(models.Model):
 
     @api.model
     def auto_transition_status(self):
-        #/
-        # Set timezone
-        # set process.wip start time (6 am - 12 pm)
-        # check if current time is in range
-        # if current time is in range:
-        #    get all records with status 'order_received' and change status to 'transitioned' 
-        # timezone = pytz.timezone('Asia/Bangkok')
-        # current_time = datetime.now(timezone).time()
-
-        # start_time = time(19, 0)
-        # end_time = time(23, 0)
-
-        # if start_time <= current_time <= end_time:
-        #     records = self.search([('status', '=', 'order_received')])
-        #     for record in records:
-                # record.write({'status': 'transitioned'})
         records = self.search([('status', '=', 'order_received')])
         for record in records:
             record.write({'status': 'transitioned'})
