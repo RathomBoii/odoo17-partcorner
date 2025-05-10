4850dfd71ccedd8480bb6a054a4a3ed8a1b7a044
RathomBoii <jessada.srmmn@gmail.com>
2025-05-10 22:14:31 +0700

    renew logic

--- odoo17-custom-addons/process_tracker/models/stock_picking_inherit.py
+++ odoo17-custom-addons/process_tracker/models/stock_picking_inherit.py
@@ -2,23 +2,30 @@ from odoo import models
 from odoo.exceptions import ValidationError
 
 
-class SockPickingInherit(models.Model):
-
+class StockPickingInherit(models.Model):
     _inherit = 'stock.picking'
 
+    def write(self, vals):
+        res = super().write(vals)  # Important to call super
+
+        if 'state' in vals and vals['state'] != 'draft': #update delivery_id only when not draft
+            for picking in self:
+                # origin is the sale order
+                if picking.origin:
+                    related_wip = self.env['process.wip'].search([
+                        ('sale_order_id.name', '=', picking.origin)  # changed from sale_order_id to sale_order_id.name
+                    ], limit=1)
+                    if related_wip:
+                        related_wip.write({'delivery_id': picking.id})
+        return res
 
     def do_print_picking(self):
         res = super().do_print_picking()
 
         for picking in self:
             related_wip = self.env['process.wip'].search([
-                (
-                    'delivery_id', '=', picking.id
-                )
-                
-            ], limit=1)
-
+                ('delivery_id', '=', picking.id)
+            ], limit=1) # added limit = 1
             if related_wip:
                 related_wip.write({'status': 'kitting'})
-
         return res
