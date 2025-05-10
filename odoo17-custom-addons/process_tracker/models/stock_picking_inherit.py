d708d036da80cb74f703ce3117f0661799e15d1e
RathomBoii <jessada.srmmn@gmail.com>
2025-05-11 00:23:43 +0700

    comment unused

--- odoo17-custom-addons/process_tracker/models/stock_picking_inherit.py
+++ odoo17-custom-addons/process_tracker/models/stock_picking_inherit.py
@@ -21,37 +21,37 @@ class StockPickingInherit(models.Model):
                     related_wip.write({'status': 'kitting'})
         return res
     
-    @api.model
-    def create(self, vals):
-        """
-        Override the create method to auto-validate the picking.
-        """
-        picking = super().create(vals)  # Create the picking first
+    # @api.model
+    # def button_validate(self, vals):
+    #     """
+    #     Override the create method to auto-validate the picking.
+    #     """
+    #     picking = super().create(vals)  # Create the picking first
 
-        # Auto-validate the picking
-        try:
-            # 1. Find the related sale order
-            sale_order_id = picking.sale_id.id
-            if sale_order_id:
-                # 2. Check if a process.wip record exists for the sale order
-                wip_record = self.env['process.wip'].search([('sale_order_id', '=', sale_order_id)], limit=1)
-                if wip_record:
-                    picking.action_confirm()
-                    picking.action_assign()
-                    if picking.state in ('assigned', 'partially_available', 'waiting', 'confirmed'):
-                        if not picking.move_line_ids:
-                            picking.action_generate_picking_lines()
-                        for move_line in picking.move_line_ids:
-                            move_line.qty_done = move_line.product_uom_qty
-                        picking.action_done()
-                else:
-                    raise UserError(f"No process.wip record found for sale order {picking.sale_id.name}.  Cannot auto-validate picking {picking.name}")
-            else:
-                raise UserError(f"No sale order found for picking {picking.name}. Cannot auto-validate.")
+    #     # Auto-validate the picking
+    #     try:
+    #         # 1. Find the related sale order
+    #         sale_order_id = picking.sale_id.id
+    #         if sale_order_id:
+    #             # 2. Check if a process.wip record exists for the sale order
+    #             wip_record = self.env['process.wip'].search([('sale_order_id', '=', sale_order_id)], limit=1)
+    #             if wip_record:
+    #                 picking.action_confirm()
+    #                 picking.action_assign()
+    #                 if picking.state in ('assigned', 'partially_available', 'waiting', 'confirmed'):
+    #                     if not picking.move_line_ids:
+    #                         picking.action_generate_picking_lines()
+    #                     for move_line in picking.move_line_ids:
+    #                         move_line.qty_done = move_line.product_uom_qty
+    #                     picking.action_done()
+    #             else:
+    #                 raise UserError(f"No process.wip record found for sale order {picking.sale_id.name}.  Cannot auto-validate picking {picking.name}")
+    #         else:
+    #             raise UserError(f"No sale order found for picking {picking.name}. Cannot auto-validate.")
 
-        except Exception as e:
-            # Handle any errors during validation
-            raise UserError(f"Error auto-validating picking {picking.name}: {e}")
-        return picking
+    #     except Exception as e:
+    #         # Handle any errors during validation
+    #         raise UserError(f"Error auto-validating picking {picking.name}: {e}")
+    #     return picking
     
     
