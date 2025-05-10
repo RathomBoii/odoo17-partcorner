37d594e3527c4054a30d13e52f9204662a523b96
RathomBoii <jessada.srmmn@gmail.com>
2025-05-10 23:14:40 +0700

    add auto delivery note validation

--- odoo17-custom-addons/process_tracker/models/stock_picking_inherit.py
+++ odoo17-custom-addons/process_tracker/models/stock_picking_inherit.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import models, api
 from odoo.exceptions import ValidationError
 
 
@@ -20,3 +20,30 @@ class StockPickingInherit(models.Model):
                 if related_wip:
                     related_wip.write({'status': 'kitting'})
         return res
+    
+    @api.model
+    def create(self, vals):
+        """
+        Override the create method to auto-validate the picking.
+        """
+        picking = super().create(vals)  # Create the picking first
+
+        # Auto-validate the picking
+        try:
+            picking.action_confirm()  # Confirm the picking
+            picking.action_assign()   # Assign the picking (reserve products)
+            # Check if the picking is ready to be processed
+            # if picking.state == 'assigned':
+            #     # Process the picking to create move lines if they don't exist
+            #     if not picking.move_line_ids:
+            #         picking.action_generate_picking_lines()
+
+            #     # Loop through each move line and set the quantity done
+            #     for move_line in picking.move_line_ids:
+            #         move_line.qty_done = move_line.product_uom_qty
+
+            #     picking.action_done()        # Validate the picking
+        except Exception as e:
+            # Handle any errors during validation
+            raise UserError(f"Error auto-validating picking {picking.name}: {e}")
+        return picking
