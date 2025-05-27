768e1a44237c0711d7c8036463af7e65a2c3d011
RathomBoii <jessada.srmmn@gmail.com>
2025-05-27 22:05:23 +0700

    remove pickup_request

--- odoo17-custom-addons/warehouse/models/__init__.py
+++ odoo17-custom-addons/warehouse/models/__init__.py
@@ -1,4 +1,4 @@
 from . import task
 from . import process_wip_inherit
-from . import pickup_request
-from . import warehouse_pickup_add_task_wizard
+# from . import pickup_request
+# from . import warehouse_pickup_add_task_wizard
